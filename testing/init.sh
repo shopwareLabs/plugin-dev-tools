334157e7081f815ee343fc1ce2545362e01184df
Simon Bäumer <s.baeumer@shopware.com>
2017-02-17 14:17:40 +0100

    Add unit and unit-coverage command

--- testing/init.sh
+++ testing/init.sh
@@ -4,10 +4,9 @@
 # Create database
 echo "create database __DB_DATABASE__" | mysql -h __DB_HOST__ -u __DB_USER__ -p__DB_PASSWORD__
 
-# Move to shopware root and setup database
 SHOPWARE_ENV=__ENV__ ant -f ./__SHOPWARE_ROOT__build/build.xml build-unit
 
-# Install SwagImportExport
+# Install plugin
 php __SHOPWARE_ROOT__bin/console sw:plugin:refresh --env="__ENV__"
 php __SHOPWARE_ROOT__bin/console sw:plugin:install --activate "__PLUGIN__" --env="__ENV__"
 
