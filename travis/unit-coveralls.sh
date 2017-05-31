6525dce3a72261a234387c7180169bfddac9f50c
Simon Bäumer <s.baeumer@shopware.com>
2017-05-31 22:23:20 +0200

    WIP

--- travis/unit-coveralls.sh
+++ travis/unit-coveralls.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-SHOPWARE_ENV=__ENV__ __SHOPWARE_ROOT__vendor/phpunit/phpunit/phpunit --coverage-clover=../../../build/log/coverage_clover.xml --verbose
+SHOPWARE_ENV=__ENV__ __SHOPWARE_ROOT__vendor/phpunit/phpunit/phpunit --coverage-clover=build/logs/coverage_clover.xml --verbose
