b0bd0fde9f84479aaf5c858e9814dc7997622270
Dennis Garding <di@shopware.com>
2020-10-07 14:04:11 +0200

    PT-12129 - Add shopware environment to make command

--- local/init.sh
+++ local/init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 cp __PLUGIN_WORKDIR__/.make.env __SHOPWARE_ROOT__.env."__ENV__"
 
+mv __PLUGIN_WORKDIR__/vendor/shopware/plugin-dev-tools/output/config_tmp.php __SHOPWARE_ROOT__config_"__ENV__".php
+
 # Create database
 I: echo "create database __DB_DATABASE__" | mysql -h __DB_HOST__ -u __DB_USER__ -p__DB_PASSWORD__
 
-make CFG=.env."__ENV__" -C __SHOPWARE_ROOT__
+make CFG=.env."__ENV__" -C __SHOPWARE_ROOT__ ENV="__ENV__"
 
 # Install plugin
 php __SHOPWARE_ROOT__bin/console sw:plugin:refresh --env="__ENV__"
